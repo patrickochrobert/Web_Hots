c79fb7ddd9b3e6c206bfe20c9c6808263f82fa79
Patrick Persson <calahatras@gmail.com>
2016-03-18 00:10:00 +0100

    If express server is hosted on Windows, the served folder path is based on last path separator (/ or \) instead of always using /.

--- server/config.js
+++ server/config.js
@@ -1,7 +1,12 @@
 module.exports = configuration();
+var path = require('path');
 
 function configuration() {
+    var l1 = __dirname.lastIndexOf('/');
+    var l2 = __dirname.lastIndexOf('\\');
+    
+    var dir = __dirname.substring(0, Math.max(l1,l2));
     return {
-        basePath: __dirname.substring(0, __dirname.lastIndexOf('/'))
+        basePath: dir
     }
 }
